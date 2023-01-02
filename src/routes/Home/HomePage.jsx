6a6e18495399f28a3ce194290daeac3c21aa1060
JulieKodehode <julie.kodehode@gmail.com>
2023-01-02 14:24:48 +0100

    Updated Home

--- src/routes/Home/HomePage.jsx
+++ src/routes/Home/HomePage.jsx
@@ -4,6 +4,9 @@ import styles from "./layout.module.css";
 // Assets
 import portrait from "../../assets/portrett_portefoelje.JPG";
 
+// Components
+import ContactPage from "../../components/Contact/ContactPage";
+
 const HomePage = () => {
 	return (
 		<main className={styles.homeMain}>
@@ -12,14 +15,14 @@ const HomePage = () => {
 			</section>
 
 			<section className={styles.sectionRight}>
-				<h1>Julie</h1>
+				<h1>Julie Hauge</h1>
 				<h3>Frontend-utvikler</h3>
-				<p>
-					{/* Edit this (taken from github) */}
+				{/* <p>
 					Jeg heter Julie, er 25 år og bor i Bergen. Begynte mitt kode eventyr Juni 2022, og begynte på
 					KodeHode August 2022. Liker å lære nye ting og lærer nye ting fort. Fritiden går til gaming,
 					boklesing og koding.
-				</p>
+				</p> */}
+				<ContactPage />
 			</section>
 		</main>
 	);
