6144ac896ef116d164f9bb7519d59f1b9e1de0f9
JulieKodehode <julie.kodehode@gmail.com>
2023-01-09 12:49:12 +0100

    Added links to contact icons.

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -5,39 +5,47 @@ const ContactPage = () => {
 	return (
 		<section className={styles.contactMain}>
 			<section className={styles.contactSection}>
-				<h3 className={styles.contactTitle}>Kontakt</h3>
+				{/* <p className={styles.contactTitle}>Kontakt</p> */}
 				<p>
 					Jeg søker praksiskplass som frontend-utvikler så kontakt meg ved interesse og ta en titt på
 					mitt arbeid frem til nå for et innblikk på mine ferdigheter.
 				</p>
 				<ul>
 					<li>
-						<img
-							className={styles.contactIcons}
-							src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-							alt="github icon"
-						/>
+						<a href="https://github.com/JulieKodehode">
+							<img
+								className={styles.contactIcons}
+								src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+								alt="github icon"
+							/>
+						</a>
 					</li>
 					<li>
-						<img
-							className={styles.contactIcons}
-							src="https://cdn-icons-png.flaticon.com/512/2213/2213470.png"
-							alt="gmail icon"
-						/>
+						<a href="https://mail.google.com/">
+							<img
+								className={styles.contactIcons}
+								src="https://cdn-icons-png.flaticon.com/512/2213/2213470.png"
+								alt="gmail icon"
+							/>
+						</a>
 					</li>
 					<li>
-						<img
-							className={styles.contactIcons}
-							src="https://cdn-icons-png.flaticon.com/512/1384/1384088.png"
-							alt="linkedin icon"
-						/>
+						<a href="https://discord.com/users/Julie.kodehode#4768">
+							<img
+								className={styles.contactIcons}
+								src="https://cdn-icons-png.flaticon.com/512/4945/4945914.png"
+								alt="discord icon"
+							/>
+						</a>
 					</li>
 					<li>
-						<img
-							className={styles.contactIcons}
-							src="https://cdn-icons-png.flaticon.com/512/1077/1077042.png"
-							alt="instagram icon"
-						/>
+						<a href="https://www.instagram.com/julieisobelle/">
+							<img
+								className={styles.contactIcons}
+								src="https://cdn-icons-png.flaticon.com/512/1077/1077042.png"
+								alt="instagram icon"
+							/>
+						</a>
 					</li>
 				</ul>
 			</section>
