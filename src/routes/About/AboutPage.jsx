6dfc060e2ec825a7d7307fb24bd4967e1f185f6e
JulieKodehode <julie.kodehode@gmail.com>
2023-01-17 11:00:30 +0100

    Updated "Om meg" and collected it to one "section"

--- src/routes/About/AboutPage.jsx
+++ src/routes/About/AboutPage.jsx
@@ -6,50 +6,35 @@ const AboutPage = () => {
 		<main id="about" className={styles.aboutMain}>
 			<section className={styles.aboutSection}>
 				<h3 className={styles.aboutTitle}>Mitt navn er Julie</h3>
+				{/* Introduction me */}
 				<p>
-					{/* Pitch fra en 21st time */}
-					Jeg går på kurs hos Kodehode. Programmering for meg er kreativitet, problemløsning og en evig
-					utvikling. Frontend er fascinerende. Det er visuelt tilfredsstillende, og viser kun en front
-					for noe mye større bak. Jeg liker å game såkalte RPG på PC, men har også en månedlig
-					sammenkomst med venner hvor vi spiller Dungeons and Dragons. <br /> <br />
-					Jeg har også en stor forkjærlighet for The Elder Scrolls spillserien, og har fullført
-					hovedhistorien i Skyrim mer enn en håndfull ganger med like mye entusiasme hver gang. Jeg liker
-					fantasybøker med mytiske kreasjoner, krig og udødelig kjærlighet, og har en dedikert Narnia
-					tatovering.
+					Jeg heter Julie, er 25 år og bor i Bergen. Begynte mitt kode eventyr Juni 2022, og begynte på
+					KodeHode August 2022. Liker å lære nye ting og lærer nye ting fort. Frontend for meg er
+					web-design, så jeg er spesielt interesert i å utvikle meg videre i CSS og design.
+				</p>
+				{/* Deeper in programming*/}
+				<p>
+					Frontend er fascinerende og for meg er det kreativitet, problemløsning og en evig utvikling.
+					Det er visuelt tilfredsstillende, og viser kun en front for noe mye større bak. Jeg liker å
+					jobbe med detaljer, og å gi detaljer en personelighet så det er mer enn ord på en side.
+				</p>
+
+				{/* Deeper in personal */}
+				<p>
+					Jeg liker å game såkalte RPG på PC, men har også en månedlig sammenkomst med venner hvor vi
+					spiller Dungeons and Dragons. Jeg har også en stor forkjærlighet for The Elder Scrolls
+					spillserien, og har fullført hovedhistorien i Skyrim mer enn en håndfull ganger med like mye
+					entusiasme hver gang. Jeg liker fantasybøker med mytiske kreasjoner, krig og udødelig
+					kjærlighet, og har en dedikert Narnia tatovering.
 				</p>
 			</section>
 
 			<section className={styles.aboutSkills}>
 				<h3 className={styles.aboutTitle}>Mine ferdigheter</h3>
-				<table>
-					{/* This gotta change somehow */}
-					<tbody>
-						<tr>
-							<td>HTML</td>
-							<td>⭐⭐⭐⭐</td>
-						</tr>
-						<tr>
-							<td>CSS</td>
-							<td>⭐⭐⭐</td>
-						</tr>
-						<tr>
-							<td>JS</td>
-							<td>⭐⭐</td>
-						</tr>
-						<tr>
-							<td>REACT</td>
-							<td>⭐⭐</td>
-						</tr>
-						<tr>
-							<td>FIGMA</td>
-							<td>⭐⭐⭐</td>
-						</tr>
-						<tr>
-							<td>GITHUB</td>
-							<td>⭐⭐⭐⭐</td>
-						</tr>
-					</tbody>
-				</table>
+				<img
+					src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX18200943.jpg"
+					alt="tag-cloud-example"
+				/>
 			</section>
 		</main>
 	);
