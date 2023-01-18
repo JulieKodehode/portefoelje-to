6bd9388829c76f316400d0845007ba8083ea4435
JulieKodehode <julie.kodehode@gmail.com>
2023-01-18 10:24:53 +0100

    Projects in Projects

--- src/routes/Projects/ProjectsPage.jsx
+++ src/routes/Projects/ProjectsPage.jsx
@@ -16,18 +16,21 @@ const ProjectPage = () => {
 
 			<section className={styles.projectGrid}>
 				<div>
-					<p>Prosjekt A</p>
+					<p className={styles.projectName}>Prosjekt A</p>
 					<img src={Countdown} alt="photo of a website" />
+					<p className={styles.photoText}>This project is xyz. Github page and github repository</p>
 					{/* https://juliekodehode.github.io/countdown/ */}
 				</div>
 				<div>
-					<p>Prosjekt B</p>
+					<p className={styles.projectName}>Prosjekt B</p>
 					<img src={Skyrim} alt="photo of a website" />
+					<p className={styles.photoText}>This project is xyz. Github page and github repository</p>
 					{/* https://juliekodehode.github.io/moduleA_skyrimProject/ */}
 				</div>
 				<div>
-					<p>Prosjekt C</p>
+					<p className={styles.projectName}>Prosjekt C</p>
 					<img src={Parallax} alt="photo of a website" />
+					<p className={styles.photoText}>This project is xyz. Github page and github repository</p>
 					{/* https://juliekodehode.github.io/moduleA_parallaxWebpage/ */}
 				</div>
 
