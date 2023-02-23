9501202c8557f7cee718013224768d523628d7b5
JulieKodehode <julie.kodehode@gmail.com>
2023-02-23 10:07:19 +0100

    Side step

---
+++ src/components/Carousel/Carosuel.jsx
@@ -0,0 +1,49 @@
+// Library
+import { useState } from "react";
+
+const ImageData = [
+	{
+		image: "https://placeimg.com/150/150/nature/sepia",
+	},
+	{
+		image: "https://placeimg.com/150/150/animals/sepia",
+	},
+	{
+		image: "https://placeimg.com/150/150/tech/sepia",
+	},
+];
+
+const Carousel = () => {
+	const [index, setIndex] = useState(0);
+
+	// Length is amount of projects (0=1)
+	const length = 4;
+
+	// previous button -1 to go back
+	const handlePrevious = () => {
+		const newIndex = index - 1;
+
+		setIndex(newIndex < 0 ? length - 1 : newIndex);
+	};
+
+	// next button +1 to go forward
+	const handleNext = () => {
+		const newIndex = index + 1;
+		setIndex(newIndex >= length ? 0 : newIndex);
+	};
+
+	return (
+		<div className="carousel">
+			<button onClick={handlePrevious}>Previous</button>
+			<p>{index}</p>
+			<>
+				{ImageData.map((slide, index) => {
+					return <img src={slide.image} alt="photo" />;
+				})}
+			</>
+			<button onClick={handleNext}>Next</button>
+		</div>
+	);
+};
+
+export default Carousel;
--- src/routes/Projects/ProjectsPage.jsx
+++ src/routes/Projects/ProjectsPage.jsx
@@ -4,6 +4,7 @@ import styles from "./layout.module.css";
 // Assets
 import Countdown from "../../assets/Projects/nedtellingTilJul.png";
 import AdviceGeneratorApp from "../../assets/Projects/adviceGeneratorApp.png";
+import Carousel from "../../components/Carousel/Carosuel";
 
 const ProjectPage = () => {
 	return (
@@ -13,46 +14,58 @@ const ProjectPage = () => {
 				<p>Et lite kollektiv av mine prosjekter fra de siste 6 månedene på KodeHode</p>
 			</section>
 
-			<section className={styles.projectGrid}>
-				<div>
-					<p className={styles.projectName}>Nedtelling til jul</p>
-					<img src={Countdown} alt="a webpage counting down to christmas" />
-					<section className={styles.photoText}>
-						<a href="https://juliekodehode.github.io/countdown/" target="_blank" rel="noreferrer">
-							page
-						</a>
-						<a href="https://github.com/JulieKodehode/countdown" target="_blank" rel="noreferrer">
-							code
-						</a>
-					</section>
-				</div>
-				<div>
-					<p className={styles.projectName}>Advice generator</p>
-					<img src={AdviceGeneratorApp} alt="a webpage generating advices" />
-					<section className={styles.photoText}>
-						<a
-							href="https://juliekodehode.github.io/advicegeneratorapp/"
-							target="_blank"
-							rel="noreferrer">
-							page
-						</a>
-						<a
-							href="https://github.com/JulieKodehode/advicegeneratorapp"
-							target="_blank"
-							rel="noreferrer">
-							code
-						</a>
-					</section>
-				</div>
-				<div>
-					<p className={styles.projectName}>Prosjekt C</p>
-					{/* <img src={} alt="" /> */}
-					<section className={styles.photoText}>
-						<a href="#home">page</a>
-						<a href="#home">code</a>
-					</section>
-				</div>
+			{/* <section className={styles.projectGrid}> */}
+			<section className={styles.projectCarousel}>
+				<button>Previous</button>
+				<section className={styles.overflow}>
+					<div className={styles.projectDisplay}>
+						<p className={styles.projectTitle}>Nedtelling til jul</p>
+						<div>{/* Carousel */}</div>
+						<p>Beskrivelse xyz</p>
+						<p className={styles.projectLinks}>
+							<a href="" target="_blank" rel="noreferrer">
+								Nettside
+							</a>
+							&
+							<a href="" target="_blank" rel="noreferrer">
+								Koden
+							</a>
+						</p>
+					</div>
+
+					<div className={styles.projectDisplay}>
+						<p className={styles.projectTitle}>Nedtelling til jul</p>
+						<div>{/* Carousel */}</div>
+						<p>Beskrivelse xyz</p>
+						<p className={styles.projectLinks}>
+							<a href="" target="_blank" rel="noreferrer">
+								Nettside
+							</a>
+							&
+							<a href="" target="_blank" rel="noreferrer">
+								Koden
+							</a>
+						</p>
+					</div>
+
+					<div className={styles.projectDisplay}>
+						<p className={styles.projectTitle}>Nedtelling til jul</p>
+						<div>{/* Carousel */}</div>
+						<p>Beskrivelse xyz</p>
+						<p className={styles.projectLinks}>
+							<a href="" target="_blank" rel="noreferrer">
+								Nettside
+							</a>
+							&
+							<a href="" target="_blank" rel="noreferrer">
+								Koden
+							</a>
+						</p>
+					</div>
+				</section>
+				<button>Next</button>
 			</section>
+			<Carousel />
 		</main>
 	);
 };
